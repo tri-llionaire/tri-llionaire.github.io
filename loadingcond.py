5134d3ec0ff1e3b7eab469ea05300b505895212f
Tristan Price <33553882+tri-llionaire@users.noreply.github.com>
2021-02-17 08:30:43 -0500

    Update loadingcond.py

--- loadingcond.py
+++ loadingcond.py
@@ -1,4 +1,4 @@
 import sys,time,os;x=0;sys.stdout.flush();s=time.time();d=int(input())
-while x < d:
+while x<d:
     x+=1;sys.stdout.write('['+'#'*x+(' '*(d-x))+']');sys.stdout.flush();time.sleep(.5);sys.stdout.write('\b'*(d+2))
 sys.stdout.write('{} seconds\n'.format(time.time()-s));sys.stdout.flush()
