846137ae30b9efa2aefb0520f770510389db9417
Tristan Price <33553882+tri-llionaire@users.noreply.github.com>
2022-01-04 09:56:56 -0500

    Create french.py

---
+++ french.py
@@ -0,0 +1,204 @@
+FRANÇAIS
+
+PRONOUNS+ETC
+i=je
+we=nous
+you=tu
+yous=vous
+he=il
+she=elle
+hes=ils
+shes=elles
+that=ça
+this=cette
+these=cettes
+me=moi (indir obj)
+you=tui (indir obj)
+it/them=lui (indir obj)
+my=mon/ma/mes (masc/fem/plur)
+your=ton/ta/tes (masc/fem/plur)
+their=son/sa/ses (masc/fem/plur)
+our=notre/nos (sing/plur)
+your=votre/vos (sing/plur) (plur)
+their=leur/leurs (sing/plur) (plur)
+ARTICLES
+the=le/la/les (masc/fem/plur)
+a=un/une (masc/fem)
+some=des
+
+PREPOSITIONS OF TIME
+before=avant
+after=après
+around=vers
+since=depuis
+(à le = au, à les = aux)
+(de le = du, de les = des)
+PREPOSITIONS OF PLACE AND MOVEMENT
+to/at=à
+in=au (masc country names)
+in=en (fem country names)
+on=dans (transport)
+from/of=de
+on=sur
+
+CONJUNCTIONS/ADVERBS/ETC
+no=non
+yes=oui
+who=qui
+what=quoi
+how=comment
+when=quand
+where=où
+why=pourquoi (for what)
+but=mais
+or=ou
+and=et
+therefore=donc
+yet=or
+neither=ni
+because=car
+then=puis
+without=sans
+than=que
+for=pour
+with=avec
+if=si
+like=comme
+by=par
+also=aussi
+so=alors
+here=ici
+over there=là
+is not=ne est pas
+sometimes=parfois
+often=souvent
+each=chaque
+
+NOUNS
+there is/are=il y a
+it is=est (o'clock)
+it is=ce est
+thing=une chose
+need=un besoin
+man=un homme
+woman=une femme
+boy=un garçon
+girl=une fille
+dog=un chien
+horse=un cheval
+cat=un chat
+cow=une vache
+chicken=
+pig=
+donkey=
+goat=
+fish=
+snake=
+lizard=
+duck=
+goose=
+bird=
+swan=
+frog=
+toad=
+whale=
+shark=
+dolphin=
+squid=
+octopus=
+shrimp=
+starfish=
+crab=
+lobster=
+scorpion=
+spider=
+mouse=
+rat=
+beetle=
+mosquito=
+gnat=
+cricket=
+grasshopper=
+monkey=
+elephant=
+ape=
+gorilla=
+zebra=
+giraffe=
+bat=
+kangaroo=
+black bear=
+grizzly bear=
+polar bear=
+koala=
+panda=
+deer=
+bison=
+buffalo=
+hippo=
+gazelle=
+moose=
+sheep=
+rhino=
+seal=
+fox=
+otter=
+cougar=
+jaguar=
+hyena=
+wolf=
+lion=
+leopard=
+cheetah=
+tiger=
+orca=
+narwhal=
+beluga=
+lynx=
+lemur=
+chimpanzee=
+orangutan=
+alligator=
+crocodile=
+turtle=
+tortoise=
+chameleon=
+salamander=
+newt=
+sea turtle=
+iguana=
+gecko=
+boa=
+python=
+cobra=
+viper=
+
+fish/insects/plants/birds
+
+ADJECTIVES
+another=autre
+some=quelque
+a few=quelques
+good=bien
+more=plus
+very=très
+many=beaucoup
+
+VERBS
+to be=être
+to have=avoir
+to go=aller
+to do=faire
+to know=savoir
+to want=vouloir
+to say=dire
+to be able to=pouvior
+to see=voir
+to come=venir
+to believe=croire
+to have to=devoir
+
+COUNTING
+one=
+two=
+three=
