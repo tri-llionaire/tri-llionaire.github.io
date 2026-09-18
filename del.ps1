2f05a385eddc95dd76628b1f6db1bc19c6831c94
Tristan Price <33553882+tri-llionaire@users.noreply.github.com>
2026-09-18 14:55:49 +0000

    Delete del.ps1

--- del.ps1
+++
@@ -1,9 +0,0 @@
-string currentDirectory = Directory.GetCurrentDirectory();
-string folderToDelete = String.Empty;
-using(OpenFileDialog d = new OpenFileDialog())
-{
-	//Do something
-	folderToDelete = Path.GetDirectoryName(d.FileName);
-}
-Directory.SetCurrentDirectory(currentDirectory);
-Directory.Delete(folderToDelete);
